05aafe924deb53b6182e80381a8047f033425a24
Joey Litalien <joey.litalien@gmail.com>
2019-06-25 14:36:14 -0400

    Removed dummy files

--- tools/run_all.sh
+++
@@ -1,6 +0,0 @@
-python3 analyze.py --ref Reference.exr \
-                   --tests PSS.exr PSS-Gaussian.exr DRM.exr \
-                   --names "PSS Kelemen" "PSS Gaussian \u03C3=0.01" "DR Trunc(50)+Gauss(0.0025)" \
-                   --metrics l2 mape mrse \
-                   --partials pssmlt_partial/ pssmlt-gaussian_partial drmlt_partial \
-                   --dir ../scenes/spaceship/
--- tools/update.sh
+++
@@ -1,9 +0,0 @@
-python3 render.py --mitsuba /home/joey/Documents/mitsuba/cmake-build-release/binaries/mitsuba \
-                  --ref ../../mitsuba/scenes/jewelry/Reference.exr \
-                  --scene ../../mitsuba/scenes/jewelry/scene_grid.xml \
-                  --dir ../scenes/jewelry/ \
-                  --name "PSSMLT" \
-                  --alg "pssmlt" \
-                  --timeout 10 \
-                  --frequency 8 \
-                  --metrics mape smape mrse
